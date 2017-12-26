8ed571ddf00ed792e6a0d4671c1a8665325d2881
Dave Ceddia <dceddia@gmail.com>
2017-12-26 12:04:12 -0500

    createStore and pass it down with Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./Counter";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+  count: 42
+};
+function reducer(state = initialState, action) {
+  return state;
+}
+const store = createStore(reducer);
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
