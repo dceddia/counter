83518e77bf1db874ba7a96073ecb21ee12f54bbc
Dave Ceddia <dceddia@gmail.com>
2018-06-19 13:07:18 -0400

    Add count and buttons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 
 class Counter extends React.Component {
   render() {
-    return <div>hi</div>;
+    return (
+      <div className="counter">
+        <button>+</button>
+        <span className="count">0</span>
+        <button>&ndash;</button>
+      </div>
+    );
   }
 }
 
